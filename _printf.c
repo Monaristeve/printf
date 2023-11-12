9ad719db99ba6c21b1dbe227bd243da05a5b0151
Mwan3kii <agathawanja303@gmail.com>
2023-11-12 14:17:58 +0300

    _printf.c

---
+++ _printf.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+/**
+ * _printf - function that produces output
+ * @format: character string
+ *
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int result = 0;
+	char c;
+	va_list args;
+	va_start(args, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+		}
+		switch (*format)
+		{
+			case 'c':
+				c = va_arg(args, int);
+				result += write(1, &c, 1);
+				break;
+			case 's':
+				result += write(1, va_arg(args, char *), strlen(va_arg(args, char *)));
+				break;
+			case '%':
+				result += write(1, "%", 1);
+				break;
+			default:
+				break;
+		}
+		format++;
+	}
+	va_end(args);
+	return (result);
+}
